de78d062bc702cd1e1b3d356cb8eb7adc5eac5b6
Lenovo-User <Lenovo-User@DESKTOP-QJ3UI1M>
2024-01-17 13:05:45 +0200

    Add project files.

---
+++ Form1.vb
@@ -0,0 +1,21 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim firstnumber, secondnumber As Integer
+
+        firstnumber = Val(TextBox1.Text)
+        secondnumber = Val(TextBox2.Text)
+
+        sum(firstnumber, secondnumber)
+
+
+    End Sub
+
+    Sub sum(a As Integer, b As Integer)
+        Dim total As Integer
+
+        total = a + b
+
+        TextBox3.Text = total
+    End Sub
+
+End Class
